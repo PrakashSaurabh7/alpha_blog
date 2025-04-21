4cad96236816cfeb563ba6f67fe3bd5f00e96018
PrakashSaurabh7 <saurabhprakash7777@gmail.com>
2025-04-21 21:37:04 +0530

    After using partials and DRY methodology

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [ :edit, :update, :show, :destroy ]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
       @var = @article.save
       if @var
         flash[:notice] = "Article was created successfully."
@@ -22,12 +22,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article updated successfully."
       redirect_to @article
     else
@@ -36,8 +34,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
